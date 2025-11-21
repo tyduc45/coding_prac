49f97973a66354b39c034b463d9d4f774aaf23e6
tyduc45 <3381274436@qq.com>
2025-11-21 20:00:31 +0800

    find >= num leftest position using binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -36,6 +36,43 @@ bool exists(std::vector<int> arr, int target)
     return false;
 }
 
+int rightLeft(std::vector<int> arr, int target)
+{
+    for (int i = 0; i < arr.size();  i++)
+    {
+        if(arr[i] >= target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+/*
+ binary search always abort elements that is not needed, so m always stands for the boundary of needed elements
+*/
+int find_big_left(std::vector<int> arr, int target)
+{
+    if (arr.size() == 0)
+        return -1;
+
+    int l = 0, r = arr.size() - 1, m = 0, ans = -1;
+    while (l <= r)
+    {
+        m = (l + r) / 2;
+        if(arr[m] >= target)
+        {
+            ans = m;
+            r = m - 1;
+        }
+        else
+        {
+            l = m + 1;
+        }
+    }
+    return ans;
+}
+
 // self-checker
 std::vector<int> randomArray(int n, int v)
 {
@@ -60,6 +97,29 @@ void printArray(const std::vector<int> &arr)
     std::cout << "\n";
 }
 
+template<typename reType>
+void runTest(reType (*binaFunc)(std::vector<int>, int), reType (*binaFunc1)(std::vector<int>, int),
+             int N, int V, int testTimes,
+             std::mt19937 rng,
+             std::uniform_real_distribution<double> dist)
+{
+    for (int i = 0; i < testTimes; i++)
+    {
+        int n = (int)(dist(rng) * N);
+        std::vector<int> arr = randomArray(n, V);
+        std::sort(arr.begin(), arr.end());
+
+        int randomNum = (int)(dist(rng) * V);
+        // they should return the same is binary search is correct
+        if (binaFunc(arr, randomNum) != binaFunc1(arr, randomNum))
+        {
+            std::cout << "the [exist] solution is wrong" << "\n";
+            printArray(arr);
+            return;
+        }
+    }
+}
+
 int main()
 {
     int N = 100;
@@ -70,21 +130,9 @@ int main()
     std::uniform_real_distribution<double> dist(0.0, 1.0);
 
     std::cout << "test starts \n";
-    for (int i = 0; i < testTimes;i++)
-    {
-        int n = (int)(dist(rng) * N);
-        std::vector<int> arr = randomArray(n,V);
-        std::sort(arr.begin(),arr.end());
-
-        int randomNum = (int)(dist(rng) * V);
-        // they should return the same is binary search is correct
-        if(right(arr,randomNum) != exists(arr,randomNum))
-        {
-            std::cout << "the [exist] solution is wrong" << "\n";
-            printArray(arr);
-            return 1;
-        }
-    }
+    runTest<bool>(right,exists,N,V,testTimes,rng,dist);
+    runTest<int>(rightLeft, find_big_left, N, V, testTimes, rng, dist);
     std::cout << "test ends \n";
+
     return 0;
 }
